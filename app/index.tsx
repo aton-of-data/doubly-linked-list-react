ae63dc1fdbe60e819806945097a046b8495535b9
aton-of-data <dornfeld.in@gmail.com>
2026-04-22 14:10:52 -0300

    feat(app): add Expo Router root layout and home route

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { DoublyLinkedListScreen } from '@/components/organisms/DoublyLinkedListScreen';
+
+export default function Index() {
+  return <DoublyLinkedListScreen />;
+}
